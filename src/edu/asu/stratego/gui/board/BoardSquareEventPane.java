bfe4f81940d49359276e59c39857a3f6a74d1d2a
nttran9 <nttran9@asu.edu>
2014-11-13 21:10:01 -0700

    fix hover validity

--- src/edu/asu/stratego/gui/board/BoardSquareEventPane.java
+++ src/edu/asu/stratego/gui/board/BoardSquareEventPane.java
@@ -5,12 +5,16 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-
+import edu.asu.stratego.game.Game;
+import edu.asu.stratego.game.GameStatus;
 import edu.asu.stratego.media.ImageConstants;
 
 public class BoardSquareEventPane extends GridPane {
     
     private ImageView hover;
+    
+    private int row;
+    private int col;
     
     public BoardSquareEventPane() {
         hover = new ImageView(ImageConstants.HIGHLIGHT_NONE);
@@ -36,11 +40,48 @@ public class BoardSquareEventPane extends GridPane {
         };
     }
     
+    private boolean isHoverValid() {
+        // TODO Player's own piece is invalid.
+        if (Game.getStatus() == GameStatus.IN_PROGRESS) {
+            
+        }
+        
+        return true;
+    }
+    
     public ImageView getHover() {
         return hover;
     }
     
     public void setPiece(Image p) {
         hover.setImage(p);
+    }
+    
+    /**
+     * @return the row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * @param row the row to set
+     */
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    /**
+     * @return the col
+     */
+    public int getCol() {
+        return col;
+    }
+
+    /**
+     * @param col the col to set
+     */
+    public void setCol(int col) {
+        this.col = col;
     }
 }
